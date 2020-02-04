13592412b71023f123fe4c22e9de5263b3ee7c2d
Michael Ross <mross.j@gmail.com>
2020-02-04 12:09:33 -0500

    remove styles.css, update app.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './components/Styles.css';
 
 const App = () => {
 	return (
