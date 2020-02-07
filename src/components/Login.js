8e20f6201ac83399a7beb06fab969b16082bd0fd
Michael Ross <mross.j@gmail.com>
2020-02-07 15:42:56 -0500

    update login style, refine layout ; apply style to register and picmetric

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import ErrorMessage from './ErrorMessage';
 import { useForm } from 'react-hook-form';
-import axios from "axios";
-import { useHistory } from "react-router-dom";
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 import { Box } from 'grommet';
 import './Login.css'
 
@@ -10,18 +10,18 @@ import './Login.css'
 const Login = () => {
 	const { register, handleSubmit, errors } = useForm();
 	const [loginError, setLoginError] = useState();
-	const baseUrl = "https://bw-pic-metric.herokuapp.com/api";
+	const baseUrl = 'https://bw-pic-metric.herokuapp.com/api';
 	const routerHistory = useHistory();
 	const onSubmit = data => {
 		axios
-    .post(baseUrl + "/auth/login", {
+    .post(baseUrl + '/auth/login', {
       email: data.email,
       password: data.password
     })
     .then(res => {
-			localStorage.setItem("USER_TOKEN", res.data.token);
-			localStorage.setItem("USER_ID", res.data.id);
-			routerHistory.push("/picmetric");
+			localStorage.setItem('USER_TOKEN', res.data.token);
+			localStorage.setItem('USER_ID', res.data.id);
+			routerHistory.push('/picmetric');
     })
     .catch(err => {
 		setLoginError('Login Error: ' + err.response.data.error.message);
@@ -47,28 +47,29 @@ const Login = () => {
 				<h1 className='loginHeader'>PicMetric</h1>
 			<div>
 			<form className='loginForm' onSubmit={handleSubmit(onSubmit)}>
-				<label>Email</label>
-				<input className='loginField'
+				<label></label>
+				<input 
 					type='email'
 					name='email'
-					placeholder=''
+					placeholder='Email'
 					ref={register({ required: true, pattern: /^\S+@\S+$/i })} />
 				<ErrorMessage error={errors.email} />
-				<label>Password</label>
-				<input className='loginField'
+				<label></label>
+				<input
 					type='password'
 					name='password'
-					placeholder=''
+					placeholder='Password'
 					ref={register({ required: true })} />
 				<ErrorMessage error={errors.password} />
-				<input classname='loginSubmit' type='submit'
-				value='Sign In' />
+				<input 
+					type='submit' className='loginButton'
+					value='Sign In' />
 				<div>{loginError}</div>
 			</form>
 			<div className='registerLink'>
-				{ "Not registered? " }
-				<span style={{ cursor: "pointer", color: '#e1106199', textDecoration: "underline"}} onClick={ e => routerHistory.push("/register") }>
-				Create an account!
+				{ 'Not registered?' }
+				<span style={{ display: 'flex', flexDirection: 'column', cursor: 'pointer', fontSize: '1.8rem', color: '#e1106199', textDecoration: 'underline'}} onClick={ e => routerHistory.push('/register') }>
+				Create an account
         		</span>
 			</div>
 			</div>
